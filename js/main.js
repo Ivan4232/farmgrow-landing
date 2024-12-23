b870c252d7e0914c83c66a2b05845f18c69639fc
Ivan4232 <vanya05202006@gmail.com>
2024-12-24 00:59:59 +0200

    Comit 22 js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+// On button click  - show menu
+// When menu opened - On button click => hide menu
 
+//Btn - mobile-menu-btn
+// Nav - main block
+
+const navElement = document.querySelector('.nav');
+const navBtnElement = navElement.querySelector('.mobile-menu-btn')
+const showMenuActiveClassName = 'nav-visible';
+
+console.log(`
+  navElement: ${navElement},
+  navBtnElement: ${navBtnElement}`);
+
+
+function toggleMenuState() {
+  
+  // Opened state
+  if (navElement.classList.contains(showMenuActiveClassName)) {
+    navElement.classList.remove(showMenuActiveClassName);
+    return;
+  }
+
+  //Close state
+  navElement.classList.add(showMenuActiveClassName);
+  }
+
+navBtnElement.addEventListener('click', toggleMenuState);
